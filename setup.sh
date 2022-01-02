f5576baa0dd008d8fa058b7c76f5be90338b5558
OlivierGaland <galand.olivier.david@gmail.com>
2022-01-02 20:11:44 +0100

    Fix +x flag in setup.sh
    Bug fix

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ apt-get update
 apt-get -y install ca-certificates curl gnupg lsb-release git
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
+apt-get update 
 apt-get -y install docker-ce docker-ce-cli containerd.io
 systemctl enable docker.service
 apt-get -y install docker-compose
